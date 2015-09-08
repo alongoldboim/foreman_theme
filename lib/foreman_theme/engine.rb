b6a6c0e8d743f528ad991cb59709d66be519f503
alongoldboim <alongoldboim@gmail.com>
2015-09-08 18:15:03 +0300

    First version of foreman theme.

--- lib/foreman_theme/engine.rb
+++ lib/foreman_theme/engine.rb
@@ -4,18 +4,39 @@ module ForemanTheme
 
   class Engine < ::Rails::Engine
     engine_name 'foreman_theme'
-
+    config.autoload_paths += Dir["#{config.root}/app/overrides"]
+    config.autoload_paths += Dir["#{config.root}/app/controllers/concerns"]
+    config.autoload_paths += Dir["#{config.root}/app/helpers"]
+    config.autoload_paths += Dir["#{config.root}/app/models/concerns"]
+    engine_peth = config.root
     # Add any db migrations
     initializer "foreman_theme.load_app_instance_data" do |app|
       app.config.paths['db/migrate'] += ForemanTheme::Engine.paths['db/migrate'].existent
     end
 
+    assets_to_precompile =
+        Dir.chdir(root) do
+          Dir['app/assets/javascripts/**/*', 'app/assets/stylesheets/**/*'].map do |f|
+            f.split(File::SEPARATOR, 4).last
+          end
+        end
+    initializer 'foreman_theme.assets.precompile' do |app|
+      app.config.assets.precompile += assets_to_precompile
+    end
+
+    initializer 'foreman_theme.override_assets' do |app|
+      unless Rails.env.production?
+        assets_to_override =
+            Dir.chdir(Gem.loaded_specs['foreman_theme'].full_gem_path) do
+              Dir['app/assets/**'].map { |d| File.absolute_path d }
+            end
+        assets_to_override.each { |path| app.assets.prepend_path path }
+      end
+    end
+
     initializer 'foreman_theme.register_plugin', :after=> :finisher_hook do |app|
       Foreman::Plugin.register :foreman_theme do
         requires_foreman '>= 1.4'
-
-        # Add a new role called 'Discovery' if it doesn't exist
-        role "ForemanTheme", [:view_foreman_theme]
 
       end
     end
@@ -45,22 +66,29 @@ module ForemanTheme
     end
 
     initializer 'foreman_theme.override_assets' do |app|
-      assets_to_override =
+      unless Rails.env.production?
+        assets_to_override =
         Dir.chdir(Gem.loaded_specs['foreman_theme'].full_gem_path) do
           Dir['app/assets/**'].map { |d| File.absolute_path d }
         end
       assets_to_override.each { |path| app.assets.prepend_path path }
+      end
     end
 
 
     #Include concerns in this config.to_prepare block
     config.to_prepare do
-      begin
-        Host::Managed.send(:include, ForemanTheme::HostExtensions)
-        HostsHelper.send(:include, ForemanTheme::HostsHelperExtensions)
-      rescue => e
-        puts "ForemanTheme: skipping engine hook (#{e.to_s})"
-      end
+      assets_to_override = [
+          "#{engine_peth}/app/assets/stylesheets",
+          "#{engine_peth}/app/assets/images",
+          "#{engine_peth}/app/assets/javascripts"]
+      assets_to_override.each { |path| Rails.application.config.assets.paths.unshift path }
+      require "directive_processor"
+      Rails.application.assets.unregister_processor('text/css', Sprockets::DirectiveProcessor)
+      Rails.application.assets.unregister_processor('application/javascript', Sprockets::DirectiveProcessor)
+      # registering the new engine override
+      Rails.application.assets.register_processor('text/css', ForemanTheme::OverrideAssets::DirectiveProcessor)
+      Rails.application.assets.register_processor('application/javascript', ForemanTheme::OverrideAssets::DirectiveProcessor)
     end
 
     rake_tasks do
@@ -73,19 +101,6 @@ module ForemanTheme
       locale_dir = File.join(File.expand_path('../../..', __FILE__), 'locale')
       locale_domain = 'foreman_theme'
       Foreman::Gettext::Support.add_text_domain locale_domain, locale_dir
-    end
-
-    initializer 'foreman_theme.overrides' do |app|
-      # pre pending plugin path to the sass @import method in order for it to search foreman_theme for files before it does it in the local dir
-      Rails.application.config.sass.load_paths << "#{config.root}/app/assets/stylesheets"
-
-      #adding the override method "include_foreman" to sprockets engine
-      require "directive_processor"
-      Rails.application.assets.unregister_processor('text/css', Sprockets::DirectiveProcessor)
-      Rails.application.assets.unregister_processor('application/javascript', Sprockets::DirectiveProcessor)
-      # registering the new engine override
-      Rails.application.assets.register_processor('text/css', ForemanTheme::OverrideAssets::DirectiveProcessor)
-      Rails.application.assets.register_processor('application/javascript', ForemanTheme::OverrideAssets::DirectiveProcessor)
     end
 
   end
